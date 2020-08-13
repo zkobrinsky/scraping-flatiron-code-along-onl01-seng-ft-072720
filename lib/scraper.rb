da38a198aa62f449bc5654e53ac1718d20af97b3
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-13 15:48:14 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,13 +15,13 @@ class Scraper
   end
 
   def make_courses
-    self.get_courses.each do |post|
-      course = Course.new
-      course.title = post.css("h2").text
-      course.schedule = post.css(".date").text
-      course.description = post.css("p").text
+      self.get_courses.each do |post|
+        course = Course.new
+        course.title = post.css("h2").text
+        course.schedule = post.css(".date").text
+        course.description = post.css("p").text
+      end
     end
-  end
 
   def print_courses
     self.make_courses
