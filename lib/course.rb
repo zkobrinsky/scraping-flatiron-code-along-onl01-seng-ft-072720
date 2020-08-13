5f1e6c2d6af6d91adaa4c29c6dcdcf6a8d26bf0d
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-13 15:22:29 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,29 @@
+require 'nokogiri'
+require 'open-uri'
+
+require_relative './scraper.rb'
 
 
+class Course
+
+  attr_accessor :title, :schedule, :description
+  @@all = []
+
+  def initialize
+    save
+    # @html = open('http://learn-co-curriculum.github.io/site-for-scraping/courses')
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+end
